77e5037397eb350320e8b4b3372fbb97a26e3cdd
luispscarvalho <luispscarvalho@gmail.com>
2015-06-05 01:17:47 -0300

    + get/set para data e valores numericos

--- src/br/edu/ifba/plugin/PROJETO/visao/Padrao.java
+++ src/br/edu/ifba/plugin/PROJETO/visao/Padrao.java
@@ -1,18 +1,41 @@
 package br.edu.ifba.plugin.PROJETO.visao;
 
+import java.util.Date;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "padrao")
 public class Padrao {
 
-	private String texto = "";
-	
+	private String texto = "texto";
+	private int selecao = 0;
+	private Date data = new Date();
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-	
+
 	public String getTexto() {
 		return texto;
 	}
-	
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public void acao() {
+		System.out.println(texto);
+	}
+
+	public int getSelecao() {
+		return selecao;
+	}
+
+	public void setSelecao(int selecao) {
+		this.selecao = selecao;
+	}
 }
